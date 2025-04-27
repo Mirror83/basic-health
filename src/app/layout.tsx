58a5f7e5f26cb028250be5bf83bbb250bb53fa5b
Glen Ochieng <glenochieng045@gmail.com>
2025-04-27 22:45:17 +0300

    feat: add Clerk authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,17 @@ import "@/styles/globals.css";
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export const metadata: Metadata = {
   title: "Basic Health",
   description: "A simple health management system prototype",
@@ -19,11 +30,41 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
-      <body>
-        {children}
-        <Toaster />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${geist.variable}`}>
+        <body>
+          <header className="flex h-16 items-center justify-between gap-4 p-4">
+            <div className="flex items-baseline gap-2">
+              <Link href="/" className="text-2xl">
+                Basic Health
+              </Link>
+            </div>
+            <div className="flex items-center gap-4">
+              <nav className="flex items-baseline">
+                <Button variant="link">
+                  <Link href="/clients" className="text-sm">
+                    Clients
+                  </Link>
+                </Button>
+                <Button variant="link">
+                  <Link href="/programs" className="text-sm">
+                    Programs
+                  </Link>
+                </Button>
+              </nav>
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </header>
+          {children}
+          <Toaster />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// The api routes are public temporarily
+const isPublicRoute = createRouteMatcher(["/", "/api(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
